10685c8f88f7fdc931592b4a2af4f0c391171a0d
aivylv <lyvialarasati@gmail.com>
2024-06-14 01:38:08 +0700

    Jika parent null (pohon kosong), node baru dijadikan ROOT

--- binarySearchTree/binarySearchTree.cpp
+++ binarySearchTree/binarySearchTree.cpp
@@ -40,6 +40,11 @@ public :
         Node* currentNode = nullptr;
         search(element, parent, currentNode); //locate the node which will be the parent of the node to be insert
 
+        if (parent == nullptr) //if the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; //Mark the new node as ROOT
+            return; //Exit
+        }
     }
 };
 
