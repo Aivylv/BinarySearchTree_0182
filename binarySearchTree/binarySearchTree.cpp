3f3c2df00d7c3394f5a43e3f3eb980673b3547e8
aivylv <lyvialarasati@gmail.com>
2024-06-14 01:36:01 +0700

    Method insert utk menambahkan node baru ke dlm pohon, mengalokasikan memori, dan menginisialisasi node baru

--- binarySearchTree/binarySearchTree.cpp
+++ binarySearchTree/binarySearchTree.cpp
@@ -28,6 +28,15 @@ public :
     {
         ROOT = nullptr; //intializing ROOT is null
     }
+
+    void insert(string element) //insert a node in the binary search tree
+    {
+        Node* newNode = new Node(element, nullptr, nullptr); //allocate memory for the new node
+        newNode->info = element; //assign value to the data field of the new node
+        newNode->leftchild = nullptr; //Make left child of the new node point to null
+        newNode->rightchild = nullptr; //Make right child of the new node point to null
+
+    }
 };
 
 int main()
